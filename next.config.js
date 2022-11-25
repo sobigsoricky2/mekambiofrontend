a9adf439029de1ea77345a164a93ba8fbe5e6bfa
ajitesh <ajitesh314mishra@gmail.com>
2022-11-25 18:06:37 +0530

    haha

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    /**
-     * Provide the locales you want to support in your application
-     */
-    locales: ["en-US", "fr-FR", "es-ES"],
-    /**
-     * This is the default locale you want to be used when visiting
-     * a non-locale prefixed path.
-     */
-    defaultLocale: "en-US",
-  },
+  
   images: {
     domains: ["test.skg-walldorf.de"],
      unoptimized: true,
