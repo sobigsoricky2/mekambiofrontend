a3d4d94417ef0bd0235ca32d6f2c2b53ef298c72
ajitesh <ajitesh314mishra@gmail.com>
2023-01-18 17:51:46 +0530

    privacy po

--- pages/privacy-policy.jsx
+++ pages/privacy-policy.jsx
@@ -15,51 +15,33 @@ const privacyPolicy = () => {
         className="partner-section rel z-1 pt-120 rpt-90 pb-75 rpb-55 container"
       >
         <h3>
-          <>
-            <br />
-            OVERVIEW
-          </>
+          <>OVERVIEW</>
         </h3>
-        <div>
-          <br />
-        </div>
-        <div>
+        <p></p>
+        <p>
           At meKambio, accessible from www.mekambio.com (the “Website”) and its
           Expert App and Customer App (the “Apps”), one of our main priorities
           is the privacy of our visitors. This Privacy Policy document contains
           types of information that is collected and recorded by meKambio and
           how we use it.
-        </div>
-        <div>
+        </p>
+        <p>
           If you have additional questions or require more information about our
           Privacy Policy, do not hesitate to contact us.
-        </div>
-        <div>
+        </p>
+        <p>
           This Privacy Policy applies only to our online activities and is valid
           for visitors to our website and apps with regards to the information
           that they shared and/or collect in meKambio. This policy is not
           applicable to any information collected offline or via channels other
           than the Website and Apps.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <>
-            1. General
-            <br />
-          </>
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
+        </p>
+
+        <>
+          <h3>1. General</h3>
+        </>
+       
+        <p>
           Anyone can access www.mekambio.com without the necessity to register
           and create a profile. Therefore, limited use of the Services meKambio
           provides is always possible without the provision of your personal
@@ -70,59 +52,52 @@ const privacyPolicy = () => {
           device, by which we track your activity on the Website and Apps. You
           can always opt out from cookies except from those, without which the
           Website and/or Apps cannot work.
-        </div>
-        <div>
+        </p>
+        <p>
           When you are a registered User, meKambio may collect from you and
           process Cookie identifiers and those data you specifically provide us
           with upon registration, including first name, last name, email, phone
           number, social network ID, time zone, interface language, IP address,
           and device type, as well.
-        </div>
-        <div>
+        </p>
+        <p>
           Specifically, if you are an expert, we may also collect and process,
           besides the aforementioned information, the other data provided by
           you, including zip-code, and education and professional backgrounds.
-        </div>
-        <div>
+        </p>
+        <p>
           Registered Users may additionally provide us with added profile
           pictures as well as chosen currency to perform actions regarding
           transactions and pay-outs.
-        </div>
-        <div>
+        </p>
+        <p>
           When we talk about “Personal Information” in this Privacy Policy, we
           are talking both about Device Information and Order Information.
-        </div>
-        <div>
+        </p>
+        <p>
           You can find out more about the specific cases of data collection and
           the purposes of processing of such data in separate section of this
           Privacy Policy.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
+        </p>
+        
         <h3>
           <>2. Data meKambio collects</>
         </h3>
-        <div>
+        <p>
           When you visit the Website, we automatically collect certain
           information about your device, including information about your web
           browser, IP address, time zone, and some of the cookies that are
           installed on your device. Additionally, as you browse the site, we
-          collect information about the individual web pages or products that
-          you view, what websites or search terms referred you to the Site, and
+          collect information about the inpidual web pages or products that you
+          view, what websites or search terms referred you to the Site, and
           information about how you interact with the Site. We refer to this
           automatically-collected information as “Device Information.”
-        </div>
-        <div>
-          <br />
-        </div>
-        <blockquote>
+        </p>
+        <p></p>
+        <>
           <h3>a. Expert Accounts</h3>
-        </blockquote>
-        <div>
+        </>
+        <p>
           meKambio collects and processes personal data submitted by you upon
           registration and further editing of your Profile. For the registration
           purposes, you submit your login credentials (username and passwords),
@@ -134,24 +109,22 @@ const privacyPolicy = () => {
           choice whether to include additional data to your profile, or not,
           such as location, phone number, language skills, education and work
           backgrounds.
-        </div>
-        <div>
+        </p>
+        <p>
           Some personal data, such as users’ names, and location that is listed
           in your user profiles or derived from IP Addresses may be publicly
           visible to other users on the Services or to the web.
-        </div>
-        <div>
+        </p>
+        <p>
           We may implement a feature that permits you to choose what information
           is made public by changing your privacy setting in “Edit My Profile”
           within the Service.
-        </div>
-        <div>
-          <br />
-        </div>
-        <blockquote>
+        </p>
+        <p></p>
+        <>
           <h3>b. Professional Accounts</h3>
-        </blockquote>
-        <div>
+        </>
+        <p>
           meKambio collects and processes personal data submitted by you upon
           registration and further editing of settings in your Account. For the
           registration purposes, you submit your login credentials (username and
@@ -160,152 +133,122 @@ const privacyPolicy = () => {
           information may help you to get more from the Services. At the same
           time, you are not required to provide additional information, not
           specifically necessary for the registration, it is your choice.
-        </div>
-        <div>c. Billing Information</div>
-        <div>
+        </p>
+        <p>c. Billing Information</p>
+        <p>
           When you make a purchase or attempt to make a purchase through the
           Website, we collect certain information from you, including your name,
           billing address, shipping address, payment information, email address,
           and phone number. &nbsp;We refer to this information as “Order
           Information.”
-        </div>
-        <div>
-          <br />
-        </div>
-        <blockquote>
+        </p>
+        <p></p>
+        <>
           <h3>c. Technologies to collect Data</h3>
-        </blockquote>
-        <div>
-          We collect Device Information using the following technologies:
-        </div>
-        <div>
+        </>
+        <p>We collect Device Information using the following technologies:</p>
+        <p>
           &nbsp; &nbsp; - “Cookies” are data files that are placed on your
           device or computer and often include an anonymous unique identifier.
           For more information about cookies, and how to disable cookies, visit
           http://www.allaboutcookies.org.
-        </div>
-        <div>
+        </p>
+        <p>
           &nbsp; &nbsp; - “Log files” track actions occurring on the Site, and
           collect data including your IP address, browser type, Internet service
           provider, referring/exit pages, and date/time stamps.
-        </div>
-        <div>
+        </p>
+        <p>
           &nbsp; &nbsp; - “Web beacons,” “tags,” and “pixels” are electronic
           files used to record information about how you browse the Site.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
+        </p>
+        <p></p>
+        <p></p>
         <h3>
           <>3. Use of Data meKambio collects</>
         </h3>
-        <div>
+        <p>
           We use the Order Information that we collect generally to fulfil any
           orders placed through the Website (including processing your payment
           information, arranging sessions or events, and providing you with
           invoices and/or order confirmations).&nbsp;
-        </div>
-        <div>
+        </p>
+        <p>
           Additionally, we use this Order Information to communicate with you;
           Screen our orders for potential risk or fraud; and when in line with
           the preferences you have shared with us, provide you with information
           or advertising relating to our services.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
+        </p>
+        <p></p>
+        <p>
           We use the Device Information that we collect to help us screen for
           potential risk and fraud (in particular, your IP address), and more
           generally to improve and optimize our Websit (for example, by
           generating analytics about how our customers browse and interact with
           the Site, and to assess the success of our marketing and advertising
           campaigns).
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
+        </p>
+        <p></p>
+        <p>
           We use Google Analytics to help us understand how our customers use
           the Website. You can read more about how Google uses your Personal
           Information here:
           &nbsp;https://www.google.com/intl/en/policies/privacy/. &nbsp;You can
           also opt-out of Google Analytics here:
           &nbsp;https://tools.google.com/dlpage/gaoptout.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
+        </p>
+        <p></p>
+        <p>
           Finally, we may also share your Personal Information to comply with
           applicable laws and regulations, to respond to a subpoena, search
           warrant or other lawful request for information we receive, or to
           otherwise protect our rights.
-        </div>
-        <div>
+        </p>
+        <p>
           As described above, we use your Personal Information to provide you
           with targeted advertisements or marketing communications we believe
           may be of interest to you. &nbsp;For more information about how
           targeted advertising works, you can visit the Network Advertising
           Initiative’s (“NAI”) educational page at
           http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
+        </p>
+       
         <h3>
           <>4. Data Retention</>
         </h3>
-        <div>
+        <p>
           When you place an order through the Website, we will maintain your
           Order Information for our records unless and until you ask us to
           delete this information.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
+        </p>
+       
         <h3>
           <>5. Do not track Data</>
         </h3>
-        <div>
+        <p>
           Please note that we do not alter our Site’s data collection and use
           practices when we see a Do Not Track signal from your browser.
-        </div>
-        <div>
-          <br />
-        </div>
-        <div>
+        </p>
+        <p></p>
+        <>
           <h3>
             <>6.&nbsp;Your rights regarding Data</>
             <>&nbsp;</>
           </h3>
-        </div>
+        </>
         <p>
           If you are a European resident, you have the right to access the
           personal information we hold about you and to ask that your personal
           information be corrected, updated, or deleted. If you would like to
           exercise this right, please contact us through the contact information
-          below.
-          <br />
-          Additionally, if you are a European resident we note that we are
-          processing your information in order to fulfil contracts we might have
-          with you (for example if you make an order through the website), or
-          otherwise to pursue our legitimate business interests listed above.
+          below. Additionally, if you are a European resident we note that we
+          are processing your information in order to fulfil contracts we might
+          have with you (for example if you make an order through the website),
+          or otherwise to pursue our legitimate business interests listed above.
           Additionally, please note that your information will be transferred
           outside of Europe, including to Canada and the United States.
         </p>
-        <p>
-          <br />
-        </p>
+    
         <h3>
           <>7.&nbsp;Privacy Policy Changes</>
         </h3>
@@ -317,12 +260,7 @@ const privacyPolicy = () => {
           changes on this page and update the Privacy Policy. The additional
           notifications on any amendments will be sent to your email.
         </p>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
+      
         <h3>
           <>8. Contact Us</>
         </h3>
@@ -331,16 +269,9 @@ const privacyPolicy = () => {
           questions, or if you would like to make a complaint, please contact us
           by e-mail at info@mekambio.com.
         </p>
-        <p>
-          <br />
-        </p>
+      
         <p>The meKambio Team</p>
-        <div>
-          <br />
-        </div>
-        <div>
-          <br />
-        </div>
+ 
       </div>
     </Layout>
   );
